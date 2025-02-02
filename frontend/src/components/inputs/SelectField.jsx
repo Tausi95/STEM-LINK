02459dd1a813a3488f1f80a9f72ead51b47c8dde
Daniel Justin <danieljmuleke@gmail.com>
2025-02-02 22:49:05 +0200

    Enhance SelectField component with Formik integration and add CaretDownIcon for improved dropdown UI

--- frontend/src/components/inputs/SelectField.jsx
+++ frontend/src/components/inputs/SelectField.jsx
@@ -1,46 +1,45 @@
 import React from "react";
-import { Field } from "formik"
+import { useField } from "formik"
+import CaretDownIcon from "../icons/CaretDownIcon";
 
 export default function SelectField({ 
-  name, 
   label, 
   options, 
   labelClassName =  "block text-gray-700 text-sm font-bold mb-2",
-  inputClassName =  "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline",
+  inputClassName =  "col-start-1 row-start-1 w-full shadow appearance-none border rounded py-2 px-3 text-gray-600 leading-tight focus:outline-none focus:shadow-outline",
   errorClassName = "text-red-500 text-xs italic text-left",
   ...props
-}) 
-{
+}) {
+  
+  const [field, meta] = useField(props);
+
   return (
-    <div className="mb-6">
-      <label className={ labelClassName }  htmlFor={name}>
+    <div className="sm:col-span-3 mb-6">
+      <label className={ labelClassName }  htmlFor={props.id || props.name}>
         {label}
       </label>
-      <Field as="select" name={name}>
-        {({ field, meta }) => (
-          <>
-            <select
-              id={name}
-              className={`${inputClassName} ${
-                meta.touched && meta.error
-                  ? "border-red-500 focus:ring-red-500"
-                  : "border-gray-300 focus:ring-blue-500"
-              }`}
-              {...field}
-              {...props}
-            >
-              {options.map((option) => (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
-            {meta.touched && meta.error && (
-              <span className={errorClassName}>{meta.error}</span>
-            )}
-          </>
-        )}
-      </Field>
+      <div className="mt-2 grid grid-cols-1">
+        <select 
+          {...field} 
+          {...props} 
+          id={props.id || props.name}
+          className={`${inputClassName} ${
+            meta.touched && meta.error
+              ? "border-red-500 focus:ring-red-500"
+              : "border-gray-300 focus:ring-blue-500"
+          }`} 
+        >
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+        <CaretDownIcon />
+      </div>
+      {meta.touched && meta.error && (
+        <span className={errorClassName}>{meta.error}</span>
+      )}
     </div>
   );
 }
